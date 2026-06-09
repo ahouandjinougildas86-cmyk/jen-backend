b363d8df6e4eb7348f52323ad46da119b0c3ef79
Giro <ahouandjinougildas86@gmail.com>
2026-06-09 05:02:31 +0100

    fix cors allow all

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const errorHandler  = require('./middlewares/errorHandler')
 
 const app = express()
 
-app.use(cors({ origin: process.env.FRONTEND_URL || '*' }))
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 
 // Routes
